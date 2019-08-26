ffb1d1477c0446357686f859c3747e3c0e2ea718
SpaceAK <akshitha4994@gmail.com>
2019-08-26 19:06:30 +0530

    SBA skeleton

---
+++ Pilot_AK/SpringBootAdmin_hcsc/src/main/java/io/springframework/artifacts/SpringBootAdmin/SpringBootAdmin_hcsc/App.java
@@ -0,0 +1,13 @@
+package io.springframework.artifacts.SpringBootAdmin.SpringBootAdmin_hcsc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
